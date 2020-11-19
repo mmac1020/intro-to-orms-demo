d1bfd15f55a05a4fc5e5a7a636eb53402e356e62
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2020-11-19 09:20:40 -0500

    Added cody and delete cody routes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const morgan = require('morgan')
 const {Dog, Owner} = require('./database/db.js')
-const {formatDogs} = require('./format/dogs')
+const {formatDog, formatDogs} = require('./format/dogs')
 const formatOwners = require('./format/owners');
 
 app.use(morgan('dev'))
@@ -42,13 +42,32 @@ app.get('/puppies', async (req, res, next) => {
     }
 })
 
-app.use((req, res) => {
-    res.status(404).send('404 NOT FOUND :( ')
+app.get('/cody-born', async (req, res, next) => {
+    try {
+        const cody = await Dog.create(
+            {
+                name: 'Cody'
+            }
+        )
+        res.send(cody);
+    }
+    catch(err) {
+        next(err);
+    }
 })
 
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('500 ERROR BOO :(')
+app.get('/delete-cody', async (req, res, next) => {
+    try {
+        const byeCody = await Dog.findOne({
+            where: {
+                name: 'Cody'
+            }
+        })
+        await byeCody.destroy();
+        res.send('Cody has been destroyed :(')
+    } catch (err) {
+        next(err);
+    }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
