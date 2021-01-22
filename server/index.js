0c5ac8e2e0f3918194ad7e216cba8d93fb7a0e95
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-01-22 08:15:37 -0500

    Removing all code for live-demo

--- server/index.js
+++ server/index.js
@@ -1,54 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const morgan = require('morgan')
-const {Dog, Owner} = require('./database/db.js')
-const {formatDogs} = require('./format/dogs')
-const formatOwners = require('./format/owners');
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.get('/', async (req, res, next) => {
-    res.send("<h1> Welcome to the main route! </h1>")
-})
-
-//EAGER LOADING just means "join"
-//select * from users join dogs on dogs.ownerId = owners.id
-app.get('/dogs', async (req, res, next) => {
-    try {
-        const dogs = await Dog.findAll({include: Owner})
-        dogs[0].sayHello()
-        res.send(formatDogs(dogs))
-    } catch(err) {
-        next(err)
-    }
-})
-
-app.get('/owners', async (req, res, next) => {
-    try {
-         const owners = await Owner.findAll()
-         res.send(formatOwners(owners))
-    } catch(err) {
-        next(err)
-    }
-})
-
-app.get('/puppies', async (req, res, next) => {
-    try {
-        const puppies = await Dog.getPuppies()
-        res.send(formatDogs(puppies))
-    } catch(err) {
-        next(err)
-    }
-})
+  res.send('<h1> Welcome to the main route! </h1>');
+});
 
 app.use((req, res) => {
-    res.status(404).send('404 NOT FOUND :( ')
-})
+  res.status(404).send('404 NOT FOUND :( ');
+});
 
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('500 ERROR BOO :(')
-})
+  console.error(err.stack);
+  res.status(500).send('500 ERROR BOO :(');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
